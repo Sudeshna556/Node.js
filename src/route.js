cb547691d348bd5832b8bd92f2d9e9bbd250ddff
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2024-11-18 18:30:21 +0530

    routing and HTTP req handlers

---
+++ src/route.js
@@ -0,0 +1,33 @@
+const express = require ("express") // import express module
+
+const app = express();  
+
+
+// handle the port
+app.use("/hello",(req,res)=>{ 
+    res.send("hello from the server");
+});
+
+app.get("/user",(req,res) =>{
+    res.send({name : "sdas", id : "66557sdas"});
+
+});
+
+app.post("/user",(req,res) =>{
+    res.send("saved to database!!");
+
+});
+
+
+app.delete("/user",(req,res) =>{
+    res.send("deleted successfully!!");
+
+});
+
+app.use("/test",(req,res)=>{ 
+    res.send("this line is just trial and to show you how to pass files, for ex /test file");
+});
+
+app.listen(3000, () => {
+    console.log("server is successfully initiated on the port number 3000..");
+});
