c30c574ceb491db0f50e67a4f36dc8d48b1000d5
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2024-11-19 21:57:17 +0530

    connect

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 const express = require ("express") // import express module
-
+const connectDB = require("./config/database");
 const app = express(); //create an instance of an Express application,this isntace stored in the app object that represents your web application
 
-//?app.listen(3000)
-//it starts our server
+connectDB() //returns a promise and it represents an asynchronous operation to connect to a database
+ .then(() =>{ //takes a callback fn which will be executed if the promise is resolved
+    console.log("database connection established..");
+    app.listen(3000, () => {
+        console.log("server is successfully initiated on the port number 3000..");
+    });
+})
+ .catch((error) =>{ //takes a callback fn which will be executed if the promise is resolved
+    console.error("database cannot be connected !!")
+ });
 
-
-
-// handle the port
-app.use("/hello",(req,res)=>{ //app.use method in express is used to mount the middleware functions, here send method is the middlewarefn.
-    res.send("hello from the server");//res.send method is used to send a response to the client
-});
-
-app.use("/test",(req,res)=>{ //app.use method in express is used to mount the middleware functions
-    res.send("sending another response!!");
-
-app.listen(3000, () => {
-    console.log("server is successfully initiated on the port number 3000..");
-});
